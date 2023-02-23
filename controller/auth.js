ba91af453d2356bf46048e68f06a38bdfb24bf07
Magesh <magesh@divum.in>
2023-02-23 16:44:53 +0530

    code

--- controller/auth.js
+++ controller/auth.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const Sort_User = require("../models/User");
 
+
+// post user data
 const postuser = async (req, res) => {
   const data = await Sort_User({
     first_name: req.body.first_name,
@@ -9,27 +11,30 @@ const postuser = async (req, res) => {
     password: req.body.password,
   });
   const user_data = await data.save();
-  //   res.status(200).send({ "User Stored : ": user_data });
   res.send(page);
 };
 
+
+
+// get user data filtered panigated 
 const allusers = async (req, res) => {
-    const filter_value ={}
+
+  const filter_value = {};
   const { size, order, filter_name } = req.body;
-  fitler_by = Object.keys(filter_name)[0]
-  console.log(fitler_by);
+  fitler_by = Object.keys(filter_name)[0];
   const page = parseInt(req.query.page) || 10;
   const pageSize = parseInt(req.query.limit) || 10;
-  console.log(pageSize);
   const skip = (page - 1) * pageSize;
   const total = await Sort_User.countDocuments();
   const pages = Math.ceil(total / pageSize);
-   filter_value[fitler_by] ={ $regex:filter_name[fitler_by], $options: "i" }
-   user_data = await Sort_User.find(filter_value)
+
+  
+  filter_value[fitler_by] = { $regex: filter_name[fitler_by], $options: "i" };
+  user_data = await Sort_User.find(filter_value)
     .skip(skip)
     .limit(pageSize)
-    .sort({"first_name":order});
-    console.log(filter_value);
+    .sort({ fitler_by: order });
+  console.log(filter_value);
   res.send({
     pages,
     page,
